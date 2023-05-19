8fe5148ff2292edf7af520ba8e6b9c7f8e5dacf4
robert <tohverrobert@gmail.com>
2023-05-19 17:52:05 +0300

    Made Knex connection into a singleton

--- database/index.js
+++ database/index.js
@@ -1,54 +1,36 @@
 const { knex } = require('knex');
 const path = require('path');
-const sampleData = require('./sample-data');
 
 const sleep = ms => new Promise(r => setTimeout(r, ms)); // TODO: take this to a utils file
 
-async function createConnection() {
-	await sleep(4000);
-	const connection = await knex({
-		client: 'pg',
-		connection: {
-			host : 'modules-db',
-			port: 5432,
-			user : 'user',
-			password : 'totally-secure-password',
-			database : 'learning-modules',
-		},
-	});
+let connection = null;
 
-	await connection.raw('SELECT 1');
+async function dbConnection() {
+	if (!connection) {
+		await sleep(4000);
 
-	await connection.migrate.latest({
+		connection = await knex({
+			client: 'pg',
+			connection: {
+				host: 'modules-db',
+				port: 5432,
+				user: 'user',
+				password: 'totally-secure-password',
+				database: 'learning-modules',
+			},
+		});
+
+		await connection.raw('SELECT 1');
+
+		await connection.migrate.latest({
 		tableName: 'knex_migrations',
 		directory: path.join(__dirname, '../migrations'),
-	});
+		});
 
-	console.log('Database connection initialised');
+		console.log('Database connection initialized');
+	}
 
 	return connection;
 }
 
-async function insertSampleData(knex) {
-	try {
-		await knex.transaction(async (trx) => {
-			for (const [key, value] of Object.entries(sampleData)) {
-				await trx(key)
-					.insert(value)
-					.onConflict('id')
-					.ignore();
-			}
-
-			console.log('Sample data inserted successfully');
-		});
-	} catch (error) {
-		console.error('Error inserting sample data:', error);
-	} finally {
-		knex.destroy();
-	}
-}
-
-module.exports = {
-	createConnection,
-	insertSampleData
-}
+module.exports = dbConnection;
